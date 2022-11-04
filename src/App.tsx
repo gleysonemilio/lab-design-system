8ee57ff702355ecd060ff2cdfa920d5c69f30f85
gleysonsi <gleysons@ciandt.com>
2022-11-04 18:40:51 -0300

    firt

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-8xl">TESTE</h1>
+      <button className="bg-cyan-500 ">teste</button>
+    </>
+  )
+}
---
+++ storybook-static/assets/preview.99a3cf17.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.86df7e01.js";import"./iframe.a2c6e1d2.js";import"./es.map.constructor.583426c3.js";import"./es.number.to-fixed.f035ca62.js";import"./index.65203840.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.99a3cf17.js.map
