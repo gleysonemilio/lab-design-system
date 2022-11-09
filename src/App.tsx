2a45979320a5ad23a47eede840677dcd6415a52f
gleysonsi <gleysons@ciandt.com>
2022-11-08 22:49:11 -0300

    feat: component and storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Heading, Text } from './components'
+import Logo from './Logo'
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-8xl">TESTE</h1>
-      <button className="bg-cyan-500 ">teste</button>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça o login e comece a usar
+        </Text>
+      </header>
+    </div>
   )
 }
